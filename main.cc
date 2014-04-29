e28d42539a7ca30df2aba54e2c1f44fc7c06ce23
Stuart Olsen <so1132@txstate.edu>
2014-04-29 05:18:46 -0500

    Added registry code

--- main.cc
+++ main.cc
@@ -10,9 +10,35 @@ handle <wl_display> get_display ()
 	return display;
 }
 
+handle <wl_registry> get_registry (handle <wl_display> display)
+{
+	auto registry = wrap_handle (wl_display_get_registry (display.get ()), wl_registry_destroy);
+	if (!registry)
+		lose ("Couldn't obtain a reference to the registry");
+
+	static const wl_registry_listener listener {
+		// Event "global"
+		[] (void*, wl_registry*, std::uint32_t id, const char* interface, std::uint32_t version) {
+			log_debug ("Registry reports that object ", std::to_string (id),
+			           " implements interface \"", interface, "\", version ",
+			           std::to_string (version));
+		},
+		// Event "global_remove"
+		[] (void*, wl_registry*, std::uint32_t id) {
+			log_debug ("Registry reports the destruction of object ", std::to_string (id));
+		}
+	};
+
+	wl_registry_add_listener (registry.get (), &listener, nullptr);
+	return registry;
+}
+
 int main ()
 {
 	auto display = get_display ();
+	auto registry = get_registry (display);
+
+	wl_display_sync (display.get ());
 
 	return EXIT_SUCCESS;
 }
